e47cd8cfacb7c34858ac5eed9d7b9b26b9269532
Marlon Couto <marloncouto@codosim.com>
2025-02-13 14:20:51 -0300

    Update server configuration to use middleware mode and implement custom server with Express

---
+++ run-server.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import { handler as ssrHandler } from './dist/server/entry.mjs';
+
+const app = express();
+// Change this based on your astro.config.mjs, `base` option.
+// They should match. The default value is "/".
+const base = '/';
+
+app.use(base, express.static('dist/client/'));
+
+app.use(ssrHandler);
+
+app.get('/api/status', (_, res) => {
+  res.json({ message: 'Server status is OK' });
+});
+
+app.listen(8080, () => console.log('Server started at http://localhost:8080'));
